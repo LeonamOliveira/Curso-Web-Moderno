cbdda38a9635d1285826c161be0332023f72be5a
Leonam Oliveira <oliveira.leonam@hotmail.com>
2019-06-21 17:53:37 -0300

    Até a aula 61

---
+++ exercicios-js/controle/doWhile.js
@@ -0,0 +1,11 @@
+function getInteiroAleatorioEntre(min, max) {
+    const valor = Math.random() * (max - min) + min
+    return Math.floor(valor)
+}
+
+do {
+    opcao = getInteiroAleatorioEntre(-1, 10)
+    console.log(`Opção escolhida foi ${opcao}.`)
+}while(opcao != -1);
+
+console.log('Até a próxima!')
