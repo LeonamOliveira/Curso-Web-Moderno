db3d48b4e1cda850c89c838022ffb0444caf0814
Leonam Oliveira <oliveira.leonam@hotmail.com>
2019-06-28 17:56:05 -0300

    Pasta objeto

---
+++ exercicios-js/objeto/oo.js
@@ -0,0 +1,16 @@
+// CÓDIGO NÃO EXECUTÁVEL
+
+// Procedural
+processamento(valor1, valor2, valor3)
+
+// OO
+objeto = {
+    valor1,
+    valor2,
+    valor3,
+    processamento() {
+        //....
+    }
+}
+
+objeto.processamento()
