4aebfd8aefbcc6ef342672078d5ac13538c90420
Leonam Oliveira <oliveira.leonam@hotmail.com>
2019-07-05 17:08:47 -0300

    Seção 7 concluída

---
+++ exercicios-js/array/reduce2.js
@@ -0,0 +1,30 @@
+const alunos = [
+    { nome: 'João', nota: 7.3, bolsista: false},
+    { nome: 'Maria', nota: 9.2, bolsista: true},
+    { nome: 'Pedro', nota: 9.8, bolsista: false},
+    { nome: 'Ana', nota: 8.7, bolsista: true}
+]
+
+// Desafio 1: Todos os alunos são bolsista?
+const todosBolsistas = (resultado, bolsista) => resultado && bolsista
+console.log(alunos.map(a => a.bolsista).reduce(todosBolsistas))
+
+// Desafio 2: Algum aluno é bolsista?
+
+const algumBolsista = (resultado, bolsista) => resultado || bolsista
+console.log(alunos.map(a => a.bolsista).reduce(algumBolsista))
+
+/*const todosBolsista = alunos.map(a => a.bolsista).reduce(function(acumulador, atual){
+    acumulador === true ? true : false
+    return acumulador
+})
+
+console.log(todosBolsista)
+
+const algumBolsista = alunos.map(a => a.bolsista).reduce(function(acumulador){
+    acumulador === true ? true : false 
+    return acumulador
+})
+
+console.log(algumBolsista)*/
+
